5621203ccb5b61ce3b8612d09f796bdbd2f25009
Anastasia <nancy56@list.ru>
2021-08-29 17:30:14 +0800

    init: upload index.html
    add media files

---
+++ script.js
@@ -0,0 +1,23 @@
+    document.addEventListener('DOMContentLoaded', function () {
+      const deadline = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 01);
+      let timerId = null;
+      function declensionNum(num, words) {
+        return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+      }
+      function countdownTimer() {
+        const diff = deadline - new Date();
+        if (diff <= 0) {
+          clearInterval(timerId);
+        }
+        const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 30 : 0;
+        const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+        $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+        $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+        $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+      }
+      const $minutes = document.querySelector('.timer__minutes');
+      const $seconds = document.querySelector('.timer__seconds');
+      countdownTimer();
+      timerId = setInterval(countdownTimer, 1000);
+    });
